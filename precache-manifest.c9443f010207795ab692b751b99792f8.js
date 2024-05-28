9eb2f9df1683759b608eb67004272b8879f574b5
Aatif-12 <aatifpothiyawale111@gmail.com>
2024-05-29 01:28:44 +0530

    Updates

---
+++ precache-manifest.c9443f010207795ab692b751b99792f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b8e706bd7e6432282c2f03cefc368e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33e89323286f32c96df7",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "741ac409fa727212323b",
+    "url": "/static/js/2.ff3a72e2.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.ff3a72e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc2123c191257a33a495",
+    "url": "/static/js/3.c86eed50.chunk.js"
+  },
+  {
+    "revision": "33e89323286f32c96df7",
+    "url": "/static/js/main.3794f3a8.chunk.js"
+  },
+  {
+    "revision": "b8db5c5b46392fa2c8c1",
+    "url": "/static/js/runtime-main.5b539819.js"
+  }
+]);
